4b526f6bf7dfd4e0a5f23b3faeff9dfbeda709c3
elisa <cattaneoelisa96@gmail.com>
2022-05-01 17:49:40 +0200

    stateful widget

--- travel_app/lib/components/card_category.dart
+++ travel_app/lib/components/card_category.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/models/interessi.dart';
 
-class CardCategory extends StatelessWidget {
+class CardCategory extends StatefulWidget {
   final Interessi? interesse;
   final bool marginRight;
 
@@ -13,25 +13,45 @@ class CardCategory extends StatelessWidget {
   const CardCategory(this.interesse, {this.color, this.icon, this.text, this.marginRight = true, Key? key}) : super(key: key);
 
   @override
+  State<CardCategory> createState() => _CardCategoryState();
+}
+
+class _CardCategoryState extends State<CardCategory> {
+  late bool selezionato;
+
+  @override
+  void initState() {
+    super.initState();
+    selezionato = false;
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      margin: marginRight ? EdgeInsets.only(right: 8) : null,
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.black54),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-                color: interesse?.color ?? color,
-                borderRadius:
-                BorderRadius.circular(16)),
-            child: Icon(interesse?.icon ?? icon),
-          ),
-          Text(interesse?.name ?? text ?? '')
-        ],
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          selezionato = !selezionato;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        margin: widget.marginRight ? const EdgeInsets.only(right: 8) : null,
+        decoration: BoxDecoration(
+          color: selezionato ? (widget.interesse?.color ?? widget.color) : null,
+            border: Border.all(color: Colors.black54),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color:selezionato ? Colors.white : (widget.interesse?.color ?? widget.color),
+                  borderRadius:
+                  BorderRadius.circular(16)),
+              child: Icon(widget.interesse?.icon ?? widget.icon),
+            ),
+            Text(widget.interesse?.name ?? widget.text ?? '')
+          ],
+        ),
       ),
     );
   }
