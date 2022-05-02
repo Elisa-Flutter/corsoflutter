4dae54411f77d4e644cf3c05781852e98d27cdb9
elisa <cattaneoelisa96@gmail.com>
2022-05-02 12:19:09 +0200

    streaming shared preferences

--- travel_app/lib/pages/preferiti_page.dart
+++ travel_app/lib/pages/preferiti_page.dart
@@ -3,51 +3,35 @@ import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 import 'package:travel_app/components/card_place.dart';
 import 'package:travel_app/models/meta_turistica.dart';
 
-class PreferitiPage extends StatefulWidget {
-  const PreferitiPage({Key? key}) : super(key: key);
+class PreferitiPage extends StatelessWidget {
+  final StreamingSharedPreferences sp;
+  const PreferitiPage(this.sp, {Key? key}) : super(key: key);
 
-  @override
-  State<PreferitiPage> createState() => _PreferitiPageState();
-}
-
-class _PreferitiPageState extends State<PreferitiPage> with SingleTickerProviderStateMixin{
-  late StreamingSharedPreferences sp;
-  List<MetaTuristica> list = [];
-
-  initSp() async{
-    sp = await StreamingSharedPreferences.instance;
-
-    final faves = (sp.getStringList("faves", defaultValue: []));
+  List<MetaTuristica> getMetePreferite(List<String> _preferences){
+    List<MetaTuristica> metePreferite = [];
     for (var meta in MetaTuristica.listaMete) {
-      if(await faves.contains(meta.city)){
-        print(meta.city);
-        list.add(meta);
+      if(_preferences.contains(meta.city)){
+        metePreferite.add(meta);
       }
     }
-    setState(() {
-    });
-  }
-
-
-  @override
-  void initState(){
-    super.initState();
-    initSp();
+    return metePreferite;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, index){
-          return AnimatedIcon(
-              progress:  AnimationController(vsync: this, duration: Duration(milliseconds: 450)),
-              icon: AnimatedIcons.event_add);
-
-          //CardPlace(list[index]);
-        }
-    ),
+      body: PreferenceBuilder<List<String>>(
+        preference: sp.getStringList('faves', defaultValue: []),
+        builder: (context, _preferenceResult){
+          List<MetaTuristica> metePreferite = getMetePreferite(_preferenceResult);
+          return ListView.builder(
+              itemCount: metePreferite.length,
+              itemBuilder: (context, index){
+                return CardPlace(metePreferite[index]);
+              }
+          );
+        },
+      )
     );
   }
 }
--- travel_app/lib/routes.dart
+++ travel_app/lib/routes.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:streaming_shared_preferences/streaming_shared_preferences.dart';
 import 'package:travel_app/pages/home.dart';
 import 'package:travel_app/pages/login_page.dart';
 import 'package:travel_app/pages/preferiti_page.dart';
 import 'package:travel_app/pages/ricerca_page.dart';
 
-generateRoutes() {
+generateRoutes(StreamingSharedPreferences sp) {
   return {
     '/' : (context) => const Home(),
     '/home' : (context) => const Home(),
     '/profile' : (context) => const Center(child: Text('Profilo')),
-    '/favorites' : (context) => const PreferitiPage(),
+    '/favorites' : (context) => PreferitiPage(sp),
     '/search' : (context) => const RicercaPage(),
   };
 }
